8d5e6b8f2d5a0679d8f59ea09deaf6289b38a8de
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-08-03 13:56:54 +0100

    Added ts func files

---
+++ popup.ts
@@ -0,0 +1,17 @@
+window.onload = () => {
+  document.getElementById("exportButton")?.addEventListener("click", () => {
+    chrome.tabs.query({}, (tabs) => {
+      const urls = tabs.map((tab) => tab.url);
+      const csvContent = urls.join("\n");
+      const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+      const link = document.createElement("a");
+      const url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", "tabs.csv");
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  });
+};
